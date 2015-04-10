f9bea81926cfd1425045947e6ed8ee66f430030d
Diana Sirenko <diana.sirenko@gmail.com>
2015-04-10 12:28:31 +0300

    Todo azure

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ var azure = require('azure-storage');
 var STORAGE_KEY = "WhayVYIGUPHk07sI/9zF6dvPNKRDnqbsRMunTVFclgzojOu8l4PNfgSJezeUF+ZDgAp+In7ryiApcRw6QVcnfA==";
 var STORAGE_ACCOUNT = "storage26";
 var blobService = azure.createBlobService(STORAGE_ACCOUNT, STORAGE_KEY);
+
+var tableService = azure.createTableService(STORAGE_ACCOUNT, STORAGE_KEY);
+
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
+var table = "todo2";
+tableService.createTableIfNotExists(table, function(error, result, response) {
+	console.log("Table create result");
+});
 
 function ensureContainer(name, callback) {
 	blobService.createContainerIfNotExists(name, {
@@ -55,6 +62,53 @@ app.post('/upload', multipartMiddleware, function(req, res, next) {
 		if(err) return next(err);
 		
 		res.redirect('/');
+	});
+});
+
+app.get('/todo/add', function(req,res,next){
+	var priority = req.query.priority;
+	var text = req.query.text;
+	
+	var entGen = azure.TableUtilities.entityGenerator;
+	var entity = {
+	  PartitionKey: entGen.Int32(priority),
+	  RowKey: entGen.String(text),
+	  priority: entGen.Int32(priority)
+	};
+	tableService.insertEntity(table, entity, function(error, result, response) {
+	  if(error) {
+		return next(error);
+	  }
+	  res.redirect('/todo');
+	});
+});
+
+app.get('/todo', function(req,res,next){
+	var is_priority = (req.query.priority);
+	var query = new azure.TableQuery();
+	var entGen = azure.TableUtilities.entityGenerator;
+	if(is_priority)
+	  query = query.where('priority ge ?', 9);
+	console.log(query);
+	tableService.queryEntities(table, query, null, function(error, result, response) {
+	
+		  if(error)
+			return next(error);
+			
+			var html = '<form action="/todo/add"><input name="priority" placeholder="Priority"><input name="text" placeholder="Task"><input type="submit" /></form>';
+			html += "<br><a href='/todo?priority=true'>Priority only</a> <a href='/todo'>All</a><br>"
+			var length = result.entries.length;
+			//console.log(length);
+			for(var i=0; i<length; ++i) {
+				var item = result.entries[i];
+				//console.log(item.PartitionKey);
+				html += item.PartitionKey._ + " " + item.RowKey._ + "<br>";
+			}
+		
+		
+		//console.log(result);
+		res.send(html);
+	  
 	});
 });
 
